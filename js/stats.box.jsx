f26c1934d3c4761493903004f4d8416876c10809
Josh Carrier <joshjcarrier@gmail.com>
2015-05-08 18:56:20 -0700

    Push individual updates to individual statistics progress bars.

--- js/stats.box.jsx
+++ js/stats.box.jsx
@@ -1,7 +1,20 @@
 var React = require('react')
 
 var StatsBox = React.createClass({
+  getInitialState: function() {
+    return {ticks: 0};
+  },
+  update: function() {
+    this.setState({ticks: this.state.ticks + 0.1});
+  },
+  componentDidMount: function() {
+    this.props.game.addUpdateListener(this);
+  },
+  componentWillUnmount: function() {
+    this.props.game.removeUpdateListener(this);
+  },
   render: function() {
+    var state = this.state;
     return (
       <div className="row edt-stats-box">
         <div className="col-md-4 label label-default">
@@ -10,8 +23,13 @@ var StatsBox = React.createClass({
 
         <div className="col-md-8">
           <div className="progress">
-            <div className="progress-bar" role="progressbar" aria-valuenow="{this.props.progress}" aria-valuemin="0" aria-valuemax="100" style={{width: this.props.progress+"%"}}>
-                <span className="sr-only">{this.props.progress}% Complete</span>
+            <div className="progress-bar" 
+                 role="progressbar" 
+                 aria-valuenow="{state.ticks}" 
+                 aria-valuemin="0" 
+                 aria-valuemax="100" 
+                 style={{width: state.ticks+"%"}}>
+                <span className="sr-only">{state.ticks}%</span>
               </div>
             </div>
           </div>
--- js/stats.list.jsx
+++ js/stats.list.jsx
@@ -2,23 +2,11 @@ var React = require('react')
 var StatsBox = require('./stats.box.jsx')
 
 var StatsList = React.createClass({
-  getInitialState: function() {
-    return {ticks: 0};
-  },
-  update: function() {
-    this.setState({ticks: this.state.ticks + 0.1});
-  },
-  componentDidMount: function() {
-    this.props.game.addUpdateListener(this);
-  },
-  componentWillUnmount: function() {
-    this.props.game.removeUpdateListener(this);
-  },
   render: function() {
-      var state = this.state;
+      var parent = this;
       var statsNodes = this.props.game.statistics.map(function (stat) {
         return (
-          <StatsBox name={stat.name} progress={stat.progress + state.ticks} />
+          <StatsBox name={stat.name} game={parent.props.game} />
         );
       });
       return (
